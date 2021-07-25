8829feb1a266b9f65641e33b69a8479a71ddd1df
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-25 18:28:10 +0100

    moved to model folder

---
+++ HospitalDataAPI/Model/DTO/TokenDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO
+{
+    public class TokenDTO
+    {
+        public string AccessToken { get; set; }
+
+    }
+}
