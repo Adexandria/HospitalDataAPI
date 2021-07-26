8c24a25d26d2a19b90557e4f092437336fc605ec
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-26 11:40:42 +0100

    created dbcontext

---
+++ HospitalDataAPI/Service/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using HospitalDataAPI.Model.LabModel;
+using HospitalDataAPI.Model.MedicationModel;
+using HospitalDataAPI.Model.PatientModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Service
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+                
+        }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<PrescribedMedication> Medication { get; set; }
+        public DbSet<LabTest> LabTest { get; set; }
+        public DbSet<LabResult> LabResult { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
