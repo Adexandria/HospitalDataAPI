b8303d5a498f59ae31764ec5b220c070fab360e7
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-27 05:59:07 +0100

    Interface for Prescribed Medication model

---
+++ HospitalDataAPI/Service/IMedication.cs
@@ -0,0 +1,23 @@
+﻿using HospitalDataAPI.Model.MedicationModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Service
+{
+    public interface IMedication
+    {
+        //PrescribedMedication
+        IEnumerable<PrescribedMedication> GetMedicationsById(Guid patientId);
+        Task<PrescribedMedication> AddMedicationById(Guid patientId);
+        Task<PrescribedMedication> UpdateMedicationById(Guid patientId);
+
+        //Medication
+        IEnumerable<Medication> GetMedications { get; }
+        Task<Medication> GetMedicationByName(string name);
+        Task<Medication> AddMedication(Medication newMedication);
+        Task<Medication> UpdateMedication(Medication updateMedication);
+
+    }
+}
