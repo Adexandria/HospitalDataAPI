2ef206a39fc971f759df480b483f03606c04cd1a
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-27 08:15:07 +0100

    created repository for lab test model

---
+++ HospitalDataAPI/Service/TestRespository.cs
@@ -0,0 +1,143 @@
+﻿using HospitalDataAPI.Model.LabModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Service
+{
+    public class TestRespository : ITest
+    {
+        readonly DataContext dataDb;
+        public TestRespository(DataContext dataDb)
+        {
+            this.dataDb = dataDb;
+        }
+
+        //LabTest
+        public async Task<LabTest> GetLabTestByCode(Guid patientId, string code)
+        {
+            try
+            {
+                if (patientId == null) throw new NullReferenceException(nameof(patientId));
+                if (string.IsNullOrWhiteSpace(code)) throw new NullReferenceException(nameof(code));
+                var labTest = await dataDb.LabTest.Where(s => s.PatientId == patientId).Where(s => s.Code.Code == code).AsNoTracking().FirstOrDefaultAsync();
+                if (labTest == null) throw new NullReferenceException(nameof(labTest));
+                return labTest;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+           
+        }
+
+        public async Task<LabTest> GetLabTestById(Guid patientId, Guid labTestId)
+        {
+            try
+            {
+                if (patientId == null) throw new NullReferenceException(nameof(patientId));
+                if (labTestId == null) throw new NullReferenceException(nameof(labTestId));
+                var labTest = await dataDb.LabTest.Where(s => s.PatientId == patientId).Where(s => s.TestId == labTestId).AsNoTracking().FirstOrDefaultAsync();
+                if (labTest == null) throw new NullReferenceException(nameof(labTest));
+                return labTest;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            
+        }
+
+        public IEnumerable<LabTest> GetLabTestsById(Guid patientId)
+        {
+            try
+            {
+                if (patientId == null) throw new NullReferenceException(nameof(patientId));
+                var labTests = dataDb.LabTest.Where(s => s.PatientId == patientId).OrderBy(s => s.TestId).AsNoTracking();
+                if (labTests == null) throw new NullReferenceException(nameof(labTests));
+                return labTests;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+           
+        }
+        public async Task AddLabTestById(Guid patientId,LabTest newLabTest)
+        {
+            try
+            {
+                if (patientId == null) throw new NullReferenceException(nameof(patientId));
+                if (newLabTest == null) throw new NullReferenceException(nameof(newLabTest));
+                await dataDb.LabTest.AddAsync(newLabTest);
+                await Save();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+          
+
+        }
+        public async Task<LabTest> UpdateLabTestById(Guid patientId,LabTest updateLabTest)
+        {
+            try
+            {
+                if (patientId == null) throw new NullReferenceException(nameof(patientId));
+                if (updateLabTest == null) throw new NullReferenceException(nameof(updateLabTest));
+
+                var currentLabTest =await GetLabTestById(patientId, updateLabTest.TestId);
+                if (currentLabTest == null) throw new NullReferenceException(nameof(currentLabTest));
+
+                dataDb.Entry(currentLabTest).State = EntityState.Detached;
+                dataDb.Entry(updateLabTest).State = EntityState.Modified;
+
+                return await GetLabTestById(patientId, updateLabTest.TestId);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        //Coding
+        public IEnumerable<Coding> GetCodings
+        {
+            get
+            {
+                return dataDb.Coding.OrderBy(s => s.TestCodeId).AsNoTracking();
+            }
+        }
+        public IEnumerable<Coding> GetCodingsByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new NullReferenceException(nameof(name));
+            return dataDb.Coding.Where(s => s.Text.Contains(name)).AsNoTracking();
+        }
+
+        //Category
+        public IEnumerable<Category> GetCategories
+        {
+            get
+            {
+                return dataDb.Category.OrderBy(s => s.CategoryId).AsNoTracking();
+            }
+        }
+        public IEnumerable<Category> GetCategoryByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new NullReferenceException(nameof(name));
+            return dataDb.Category.Where(s => s.Code.Contains(name)).AsNoTracking();
+        }
+
+        private async Task Save() 
+        {
+            await dataDb.SaveChangesAsync();
+        }
+    }
+}
