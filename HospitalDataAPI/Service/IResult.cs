dea72a3d16d7604cec88e360fe06803647db0dbf
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-27 05:59:37 +0100

    interface for lab result model

---
+++ HospitalDataAPI/Service/IResult.cs
@@ -0,0 +1,16 @@
+﻿using HospitalDataAPI.Model.LabModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Service
+{
+   public interface IResult
+    {
+        IEnumerable<LabResult> GetLabResultById(Guid patientId);
+        Task<LabResult> GetLabResult(Guid patientId, int testId);
+        Task<LabResult> AddLabResultId(Guid patientId);
+        Task<LabResult> UpdateLabResultId(Guid patientId);
+    }
+}
