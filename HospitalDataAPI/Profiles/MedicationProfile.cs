6a9dec3ccfac868c0f157b1c54a9bae7d431e8a2
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 07:18:22 +0100

    created profile for medication

---
+++ HospitalDataAPI/Profiles/MedicationProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HospitalDataAPI.Model.DTO.Medications;
+using HospitalDataAPI.Model.MedicationModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Profiles
+{
+    public class MedicationProfile :Profile
+    {
+        public MedicationProfile()
+        {
+            CreateMap<PrescribedMedication, PrescribedMedsDTO>()
+                .ForMember(s => s.Status, opt => opt.MapFrom(s => s.Status.ToString()))
+                .ForMember(s => s.Date, opt => opt.MapFrom(s => s.Date.Date.ToString()));
+            CreateMap<PrescribedMedsUpdate, PrescribedMedication>();
+            CreateMap<PrescribedMedsCreate, PrescribedMedication>();
+        }
+    }
+}
