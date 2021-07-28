2f15f498e4e14b70878c3f4b21aebd354d61a075
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 07:52:05 +0100

    updated

--- HospitalDataAPI/Model/LabModel/LabResult.cs
+++ HospitalDataAPI/Model/LabModel/LabResult.cs
@@ -12,13 +12,13 @@ namespace HospitalDataAPI.Model.LabModel
     {
         [Key]
         public Guid ResultId { get; set; }
-        public LabStatus Status { get; set; }
+        public LabStatus Status { get; set; } = LabStatus.Pending;
         [ForeignKey("Code")]
         public int CodeId { get; set; }
         public virtual Coding Code { get; set; }
         public string ResultValue { get; set; }
         public string ReferenceRange { get; set; } = "5.0-8.0";
-        public DateTime ResultTime { get; set; }
+        public DateTime ResultTime { get; set; } = DateTime.Now;
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
--- HospitalDataAPI/Model/LabModel/LabTest.cs
+++ HospitalDataAPI/Model/LabModel/LabTest.cs
@@ -9,15 +9,15 @@ namespace HospitalDataAPI.Model.LabModel
     {
         [Key]
         public Guid TestId { get; set; }
-        public LabStatus Status { get; set; }
+        public LabStatus Status { get; set; } = LabStatus.Pending;
         [ForeignKey("TestCode")]
         public int CodeId { get; set; }
         public virtual Coding Code { get; set; }
-        public DateTime MeasuredTime { get; set; }
+        public DateTime MeasuredTime { get; set; } = DateTime.Now;
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
-        public DateTime ReportedDate { get; set; }
+        public DateTime ReportedDate { get; set; } = DateTime.Now;
         [ForeignKey("Patient")]
         public Guid PatientId { get; set; }
         public Patient Patient { get; set; }
