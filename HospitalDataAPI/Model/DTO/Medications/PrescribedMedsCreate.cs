8c644403cd73682aaffce622035cc911a930b068
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 06:37:54 +0100

    create Create and Update DTO

---
+++ HospitalDataAPI/Model/DTO/Medications/PrescribedMedsCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO.Medications
+{
+    public class PrescribedMedsCreate
+    {
+        [Required(ErrorMessage ="Enter Date(yyyy/mm/dd)")]
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Enter Final/Pending")]
+        public string Status { get; set; }
+        [Required(ErrorMessage = "Enter Presciber")]
+        public string Prescriber { get; set; }
+       
+    }
+}
---
+++ HospitalDataAPI/Model/DTO/Medications/PrescribedMedsUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO.Medications
+{
+    public class PrescribedMedsUpdate
+    {
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Prescriber { get; set; }
+    }
+}
