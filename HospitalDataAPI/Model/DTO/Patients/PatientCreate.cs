aec9b97305b270a31a84a383ea1bff4412fd4962
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 06:24:25 +0100

    created Create and Update DTO

---
+++ HospitalDataAPI/Model/DTO/Patients/PatientCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HospitalDataAPI.Model.DTO.Patients
+{
+    public class PatientCreate
+    {
+        [Required(ErrorMessage ="Enter FirstName")]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required(ErrorMessage = "Enter LastName")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Enter Date of Birth (yyyy/mm/dd)")]
+        public DateTime DateOfBirth { get; set; }
+        public string Phonenumber { get; set; }
+        public string AddressBox { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Race { get; set; }
+        public string BirthSex { get; set; }
+    }
+}
---
+++ HospitalDataAPI/Model/DTO/Patients/PatientUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace HospitalDataAPI.Model.DTO.Patients
+{
+    public class PatientUpdate
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phonenumber { get; set; }
+        public string AddressBox { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Race { get; set; }
+        public string BirthSex { get; set; }
+    }
+}
