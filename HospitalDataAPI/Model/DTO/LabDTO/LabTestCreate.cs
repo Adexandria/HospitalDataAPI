9043f9b8c26e68766df496f239b74453f9c119b3
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 06:43:12 +0100

    created Update and Create dto

---
+++ HospitalDataAPI/Model/DTO/LabDTO/LabTestCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO.LabDTO
+{
+    public class LabTestCreate
+    {
+        [Required(ErrorMessage ="Enter Final/Pending")]
+        public string Status { get; set; }
+        [Required(ErrorMessage = "Enter measured time (yyyy/mm/dd)")]
+        public DateTime MeasuredTime { get; set; }
+        [Required(ErrorMessage = "Enter reported time (yyyy/mm/dd")]
+        public DateTime ReportedDate { get; set; }
+
+    }
+}
---
+++ HospitalDataAPI/Model/DTO/LabDTO/LabTestUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO.LabDTO
+{
+    public class LabTestUpdate
+    {
+        public Guid TestId { get; set; }
+        public string Status { get; set; }
+        public DateTime MeasuredTime { get; set; }
+        public DateTime ReportedDate { get; set; }
+    }
+}
