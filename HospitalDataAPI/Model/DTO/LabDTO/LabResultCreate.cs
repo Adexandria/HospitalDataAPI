cc98d542943faf72a6061f1c31030e85ee034165
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 06:50:27 +0100

    created create and update dto

---
+++ HospitalDataAPI/Model/DTO/LabDTO/LabResultCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Model.DTO.LabDTO
+{
+    public class LabResultCreate
+    {
+        [Required(ErrorMessage ="Enter Final/Pending")]
+        public string Status { get; set; }
+        [Required(ErrorMessage = "Enter result Value")]
+        public string ResultValue { get; set; }
+        [Required(ErrorMessage = "Enter reference range")]
+        public string ReferenceRange { get; set; }
+        [Required(ErrorMessage = "Enter result time (yyyy/mm/dd)")]
+        public DateTime ResultTime { get; set; }
+ 
+    }
+}
