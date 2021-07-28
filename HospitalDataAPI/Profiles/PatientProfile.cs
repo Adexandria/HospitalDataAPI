853ebd1f4550530bd1f9c9d2c21b87b4f74ee0c9
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 07:13:50 +0100

    Created a profile for patient Model

---
+++ HospitalDataAPI/Profiles/PatientProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HospitalDataAPI.Model.DTO.Patients;
+using HospitalDataAPI.Model.PatientModel;
+using HospitalDataAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalDataAPI.Profiles
+{
+    public class PatientProfile :Profile
+    {
+        public PatientProfile()
+        {
+            CreateMap<Patient, PatientDTO>()
+                .ForMember(s => s.Age, opt => opt.MapFrom(s => s.DateOfBirth.GetCurrentAge()))
+                .ForMember(s => s.BirthSex, opt => opt.MapFrom(s => s.BirthSex.ToString()))
+                .ForMember(s => s.Race, opt => opt.MapFrom(s => s.Race))
+                .ForMember(s => s.MaritalStatus, opt => opt.MapFrom(s => s.MaritalStatus.ToString()))
+                .ForMember(s=>s.Gender,opt=>opt.MapFrom(s=>s.Gender.ToString()));
+            CreateMap<Patient, PatientsDTO>()
+                .ForMember(s => s.Name, opt => opt.MapFrom(s => $"{s.FirstName } {s.MiddleName} {s.LastName}"));
+            CreateMap<PatientCreate, Patient>();
+            CreateMap<PatientUpdate, Patient>();
+        }
+    }
+}
