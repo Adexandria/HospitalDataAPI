3c851b41aa29f9365c89402fea9bd4b8b1253acc
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-28 07:52:35 +0100

    an extension to convert date of birth to age

---
+++ HospitalDataAPI/Service/DateTimeOffsetExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HospitalDataAPI.Service
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTime dateTime)
+        {
+            var currentDate = DateTime.UtcNow;
+            int age = currentDate.Year - dateTime.Year;
+
+            if (currentDate < dateTime.AddYears(age))
+            {
+                age--;
+            }
+            
+            return age;
+        }
+    }
+}
