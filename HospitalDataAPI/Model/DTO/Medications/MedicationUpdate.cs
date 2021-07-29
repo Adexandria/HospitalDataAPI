2d9c6f502bb0dab4598220d09f2febe2cb17205b
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-29 13:02:26 +0100

    removed the medicationId property

--- HospitalDataAPI/Model/DTO/Medications/MedicationUpdate.cs
+++ HospitalDataAPI/Model/DTO/Medications/MedicationUpdate.cs
@@ -4,7 +4,7 @@ namespace HospitalDataAPI.Model.DTO.Medications
 {
     public class MedicationUpdate
     {
-        public int MedicationId { get; set; }
+        
         public string Code { get; set; }
         public string Display { get; set; }
     }
