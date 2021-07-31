533f7fe952cf5da7aa067f8aaba0b78a2ba6748a
Adexandria <adeolaaderibigbe09@gmail.com>
2021-07-31 10:18:26 +0100

    updated

--- HospitalDataAPI/Model/LabModel/LabResult.cs
+++ HospitalDataAPI/Model/LabModel/LabResult.cs
@@ -15,18 +15,18 @@ namespace HospitalDataAPI.Model.LabModel
         public LabStatus Status { get; set; } = LabStatus.Pending;
         [ForeignKey("Code")]
         public int CodeId { get; set; }
-        public virtual Coding Code { get; set; }
+        public  Coding Code { get; set; }
         public string ResultValue { get; set; }
         public string ReferenceRange { get; set; } = "5.0-8.0";
         public DateTime ResultTime { get; set; } = DateTime.Now;
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
+        public  Category Category { get; set; }
         [ForeignKey("LabTest")]
         public Guid TestId { get; set; }
-        public virtual LabTest LabTest { get; set; }
+        public LabTest LabTest { get; set; }
         [ForeignKey("Patient")]
         public  Guid PatientId { get; set; }
-        public virtual Patient Patient { get; set; }
+        public  Patient Patient { get; set; }
     }
 }
